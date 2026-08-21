e30a16d06ba5980af9fc7ac6af90a85cc4224706
Dawit Gebremichael <dtgdev@gmail.com>
2026-08-21 11:40:08 -0500

    feat: add scientific literature runtime package

---
+++ apps/api/app/scientific_literature/__init__.py
@@ -0,0 +1,3 @@
+from app.scientific_literature.pubmed import PubMedArticle, PubMedClient, ingest_pubmed_article, parse_pubmed_xml
+
+__all__ = ["PubMedArticle", "PubMedClient", "ingest_pubmed_article", "parse_pubmed_xml"]
